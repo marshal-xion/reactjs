27f131565158e58584b58abc1eb2fd1881a0006b
B <konta1231@gmail.com>
2025-04-26 15:26:28 +0530

    added image and css

--- app2/app2/src/About.jsx
+++ app2/app2/src/About.jsx
@@ -1,11 +1,15 @@
 import { Routes, Route, useNavigate } from 'react-router-dom';
 
+const user = {
+    name: 'Hedy Lamarr',
+    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+    imageSize: 90,
+  };
+
 
 function MyButton() {
 
     const navigate = useNavigate(); // Hook to navigate programmatically
-  
-   
   
     return (
     <div className="about-container">
@@ -19,13 +23,23 @@ function MyButton() {
 
 
 
-
-
 export default function About() {
     return (
       <div>
         <h1>About Page</h1>
         <p>This is the About page of my app.</p>
+
+        <h1>{user.name}</h1>
+
+        <img
+            className="avatar"
+            src={user.imageUrl}
+            alt={'Photo of ' + user.name}
+            style={{
+            width: user.imageSize,
+            height: user.imageSize
+            }}
+        />
 
         <Routes>
           <Route path="/" element={<MyButton />} />
